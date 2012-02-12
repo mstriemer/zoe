36c6cf23957fe669e217a649e4f44d1491a98094
Mark Striemer <mstriemer@gmail.com>
2012-02-12 17:10:34 -0600

    Move settings to a module, ignore settings.local

--- zoe/settings/base.py
+++ zoe/settings/base.py
@@ -1,6 +1,6 @@
 import os
 
-PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
 root_path = lambda p: os.path.join(PROJECT_ROOT, p)
 
